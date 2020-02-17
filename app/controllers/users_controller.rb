d75bde23926dc934a63cbee29bc14a3d92f75c54
Riley Slayden <slaydenriley@gmail.com>
2020-02-17 02:05:17 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,9 @@
 class UsersController < ApplicationController
   get '/signup' do
-    erb :'users/create_user'
+    if !logged_in?
+      erb :'users/create_user', locals: {message: "Please sign up before you sign in"}
+    else
+      redirect to '/tweets'
+    end
   end
-
 end
